b98d4a796c87afc40dcb00d200e4b724dd509173
JaTynan <jtynan@outlook.com.au>
2024-08-24 08:15:20 +1000

    commit

--- main.go
+++ main.go
@@ -33,6 +33,12 @@ func (s *stack) IsEmpty() bool {
 
 func main() {
 
+	// Array to hold json file contents
+	var items [100][100][100]any
+	itemsX := 0
+	itemsY := 0
+	itemsZ := 0
+
 	// Opening the file
 	fileName := "response.json"
 	fmt.Printf("\nAttempting to open file named:%v\n", fileName)
@@ -50,18 +56,33 @@ func main() {
 	delimiterCharacters := "{}[],:\""
 	delimiterRunes := []rune(delimiterCharacters)
 	fmt.Printf("\nDelimiter Characters: %v\n", delimiterCharacters)
+
+	quotationOuterCount := 0
+	quotationInnerCount := 0
+	quotationInnerInnerCount := 0
 	for {
+		// check for when we hit the end of the file, run out of characters to process
 		char, _, err := reader.ReadRune()
 		if err != nil {
 			break
 		}
-		fmt.Printf("%c", char)
-		if char == delimiterRunes[0] {
+		// This is where we process the characters and set the array
+		// Start by handling the depth
+		if char == delimiterRunes[0] || char == delimiterRunes[2] {
 			bracketStack.Push(char)
-		} else if char == delimiterRunes[1] {
+		} else if char == delimiterRunes[1] || char == delimiterRunes[3] {
 			bracketStack.Pop()
 		}
+		if char == delimiterRunes[6] {
+			quotationOuterCount += 1
+			quotationInnerCount += 1
+			quotationInnerInnerCount += 1
+		}
+		itemsX = len(bracketStack.items)
+		itemsY = 0
+		itemsZ = 0
+		items[itemsX][itemsY][itemsZ] = string(char)
 	}
-
 	fmt.Printf("\nFinished reading the file named:%v", fileName)
+	fmt.Printf("\nArray from JSON built:\n%v", items)
 }
