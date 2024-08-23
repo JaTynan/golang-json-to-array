3ca2488be3dca20f022177a2389a6d44e2eb14e1
JaTynan <jtynan@outlook.com.au>
2024-08-23 16:21:12 +1000

    adding stack tracking for delimiter tracking

--- main.go
+++ main.go
@@ -6,26 +6,61 @@ import (
 	"os"
 )
 
+type stack struct {
+	items []interface{}
+}
+
+func (s *stack) Push(v interface{}) {
+	s.items = append(s.items, v)
+}
+func (s *stack) Pop() interface{} {
+	if len(s.items) == 0 {
+		return nil
+	}
+	last := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return last
+}
+func (s *stack) Peek() interface{} {
+	if len(s.items) == 0 {
+		return nil
+	}
+	return s.items[len(s.items)-1]
+}
+func (s *stack) IsEmpty() bool {
+	return len(s.items) == 0
+}
+
 func main() {
 
 	// Opening the file
 	fileName := "response.json"
-	fmt.Printf("\nAttemtping to open file named:%v", fileName)
+	fmt.Printf("\nAttempting to open file named:%v\n", fileName)
 	file, err := os.Open(fileName)
 	if err != nil {
-		fmt.Printf("\nError opening file:%v", err)
+		fmt.Printf("\nError opening file:%v\n", err)
 		return
 	}
 	defer file.Close()
 
 	// Reading the file
 	reader := bufio.NewReader(file)
+	var bracketStack stack
+	//  {==0  }==1  [==2  ]==3 ,==4  :==5  "==6
+	delimiterCharacters := "{}[],:\""
+	delimiterRunes := []rune(delimiterCharacters)
+	fmt.Printf("\nDelimiter Characters: %v\n", delimiterCharacters)
 	for {
 		char, _, err := reader.ReadRune()
 		if err != nil {
 			break
 		}
 		fmt.Printf("%c", char)
+		if char == delimiterRunes[0] {
+			bracketStack.Push(char)
+		} else if char == delimiterRunes[1] {
+			bracketStack.Pop()
+		}
 	}
 
 	fmt.Printf("\nFinished reading the file named:%v", fileName)
